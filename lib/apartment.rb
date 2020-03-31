e3be2e16d4250100719c795c6ebe89a1d78b93bd
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-31 10:00:04 -0600

    Create add renter method and test

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,11 +1,20 @@
 class Apartment
   attr_reader :number,
               :monthly_rent,
-              :bedrooms
+              :bathrooms,
+              :bedrooms,
+              :renter
 
   def initialize(apartment_params)
     @number = apartment_params[:number]
     @monthly_rent = apartment_params[:monthly_rent]
+    @bathrooms = apartment_params[:bathrooms]
     @bedrooms = apartment_params[:bedrooms]
+    @renter = nil
   end
+
+  def add_renter(renter)
+    @renter = renter
+  end
+
 end
